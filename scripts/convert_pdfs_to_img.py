665d3c53461a8491802dc31c8402038f45e060f7
Zhang Hongyi <zhang.hongyi@outlook.com>
2016-11-03 10:30:38 -0700

    updated scripts

--- scripts/convert_pdfs_to_img.py
+++ scripts/convert_pdfs_to_img.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-#from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfFileWriter, PdfFileReader
 import argparse, os
-
+import wget
 from tqdm import tqdm
 import requests
 
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
-#import PythonMagick
+from wand.image import Image
+from multiprocessing import Pool
 
 """
 This script primarily acts
@@ -18,49 +19,78 @@ TEMP_DIR = ".tmp"
 FOLDER_TEMPLATE = "reel-%02d"
 FILE_TEMPLATE = "reel-%02d.pdf"
 REEL_LINK_TEMPLATE = 'https://ia802704.us.archive.org/10/items/ucladailybruin%02dlosa/ucladailybruin%02dlosa.pdf'
-
+PAGE_FILE_TEMPLATE = "page-%03d.pdf"
 
 PDF_REPO_FOLDER_ID = "0B9y1-prT44zAcTFmWGtrNXZobGc"
 
-
 class Reel:
-    def __init__(self, reel_no, gdrive_folder):
-        self.folder_id = gdrive_folder
-        self.reel_path = os.path.join(TEMP_DIR, FOLDER_TEMPLATE % reel_no)
+    def __init__(self, reel_no):
+        self.reel_path = os.path.join(TEMP_DIR, FILE_TEMPLATE % reel_no)
         self.reel_no = reel_no
+        self.split_output_dir = os.path.join(TEMP_DIR, FOLDER_TEMPLATE % self.reel_no)
+        self.gdrive = GDrive()
+        self.folder_id = self.gdrive.create_folder_if_not_exists(FOLDER_TEMPLATE % self.reel_no)
 
         #im gonna assume the file isn't corrupted
         if not os.path.isfile(self.reel_path):
             url = REEL_LINK_TEMPLATE % (reel_no, reel_no)
-            response = requests.get(url, stream=True)
-            print "Downloading reel %d..." % self.reel_no
-            with open(os.path.join(TEMP_DIR, FILE_TEMPLATE % reel_no), "wb") as handle:
-                for data in tqdm(response.iter_content()):
-                    handle.write(data)
+            print "Downloading reel %d from %s..." % (self.reel_no, url)
+            f_name = wget.download(url)
+            os.rename(os.path.join(os.getcwd(), f_name), self.reel_path)
 
     def split_reel(self):
-        f = PdfFileReader(open(path, 'rb'))
-        output_dir = os.path.join(TEMP_DIR, str(hash(path)))
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+        print "Splitting reel %d..." % self.reel_no
+        f = PdfFileReader(open(self.reel_path, 'rb'))
 
-        parse_text(os.path.join(output_dir, "page-03.pdf"))
-        return 0
-        for pnum in xrange(f.getNumPages()):
+        if os.path.exists(self.split_output_dir):
+            return
+
+        os.makedirs(self.split_output_dir)
+
+        for pnum in tqdm(xrange(f.getNumPages())):
             page = f.getPage(pnum)
-            print page.extractText()
             out = PdfFileWriter()
             out.addPage(page)
-            outfile = open(os.path.join(output_dir, "page-%02d.pdf" % pnum), 'wb')
+            outfile = open(os.path.join(self.split_output_dir, PAGE_FILE_TEMPLATE % pnum), 'wb')
             out.write(outfile)
-            print "Successfully written Page %d..." % pnum
+
+    @staticmethod
+    def convert_image_and_upload(img_path, img_fname, folder_id, gdrive):
+        if not img_fname.endswith('.pdf'):
+            return
+        if os.path.exists(img_path+'.jpg'):
+            print "%s exists! Skipping..." % img_path
+            return
+        pdf_pages = Image(filename=img_path, resolution=144)
+        page = pdf_pages.sequence[0]
+        with Image(page) as i:
+            i.format = 'jpg'
+            i.crop(width=i.width, height=int(0.3*i.height), gravity="north")
+            i.save(filename=img_path+'.jpg')
+            print "Converted image %s! Uploading to Gdrive..." % img_path
+            gdrive.upload(img_path+'.jpg', img_fname+'.jpg', folder_id)
+            gdrive.upload(img_path, img_fname, folder_id)
+
+    def convert_images_and_upload(self):
+        print "Converting images..."
+        process_pool = Pool()
+        args = [(os.path.join(self.split_output_dir,f), f, self.folder_id, self.gdrive) for f in os.listdir(self.split_output_dir)]
+        process_pool.map(call_reel_convert, args)
+        #for p in :
+        #    if p.endswith('.pdf'):
+        #        self.__convert_image_and_upload(os.path.join(self.split_output_dir,p), p)
+        #        break
+
+def call_reel_convert(arg):
+    Reel.convert_image_and_upload(arg[0], arg[1], arg[2], arg[3])
 
 class GDrive:
     def __init__(self):
         self.gauth = GoogleAuth()
         self.gauth.LoadCredentialsFile("cred.txt")
         if self.gauth.credentials is None:
-            self.gauth.LocalWebserverAuth()
+            #self.gauth.LocalWebserverAuth()
+            self.gauth.CommandLineAuth()
         elif self.gauth.access_token_expired:
             self.gauth.Refresh()
         else:
@@ -83,7 +113,9 @@ class GDrive:
     def upload(self, path, fname, folderid):
         f = self.drive.CreateFile({'title': fname,
             "parents": [{"id": folderid}]})
-        print f
+        f.SetContentFile(path);
+        f.Upload()
+        print "Uploaded %s to GDrive!" % path
 
 def handle_input():
     msg = """
@@ -99,11 +131,9 @@ def handle_input():
     if not os.path.exists(TEMP_DIR):
         os.makedirs(TEMP_DIR)
 
-    drive = GDrive()
-    folder_id = drive.create_folder_if_not_exists(FOLDER_TEMPLATE % args.reel_number)
-
-    reel = Reel(reel_number, folder_id)
-    #reel_path = FILE_TEMPLATE % args.reel_number
+    reel = Reel(reel_number)
+    reel.split_reel()
+    reel.convert_images_and_upload()
     #split_reel(os.path.join("reels", reel_path))
 
 
