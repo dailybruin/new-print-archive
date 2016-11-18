a674011f2eb5f0f1fab700b1d5680d44a7be595d
Andrew Yoon <andrewy563@gmail.com>
2016-11-17 16:39:54 -0800

    added sorting based on y coordinate

--- scripts/visionscript.py
+++ scripts/visionscript.py
@@ -1,13 +1,10 @@
 from base64 import b64encode
-from os import makedirs
-from os.path import join, basename
-from sys import argv
 
 import requests
 import json
 
-image_filename= 'page-004.pdf.header.jpg'
-api_key= 'AIzaSyD6kpyzItHfRlJNEmRTFRF5HDdCNXMYPhQ'
+image_filename= 'C:/Users/Andrew/Documents/dailybruin/archive/page-004.pdf.header.jpg' # file name goes here
+api_key= 'AIzaSyD6kpyzItHfRlJNEmRTFRF5HDdCNXMYPhQ'                                     # API key goes here
 
 def make_image_data_list(image_filenames):
     img_request = []
@@ -33,13 +30,26 @@ def analyze() :
         headers={'Content-Type': 'application/json'})
     return out
 
+def extract_y(data) :
+    try:
+        out=data['boundingPoly']['vertices'][1]['y']
+    except KeyError:
+        print("ERROR", end='\n')
+        return 0
+    return out
+
+def sort(data) :        # main problem: what if the dates are on the same y coordinate as another string?
+    out = sorted(data.json()['responses'][0]['textAnnotations'], key=extract_y)
+    return out;
+
 if __name__ == '__main__': 
     response=analyze()
-
     if response.status_code != 200 or response.json().get('error'):
         print(response.text)
     else:
-        for i in range(len(response.json()['responses'][0]['textAnnotations'])):
-            type(i)
-            t=response.json()['responses'][0]['textAnnotations'][i]['description']
-            print(t, end=" ")
+        data=sort(response)
+        for i in range(1, len(data)) :
+            # the first element in the json is all lines of text in the json, separated by a \n. Delete the '1,' in the range()
+            # if you need the first element.
+            t=data[i]['description']
+            print(t, end="\n")
